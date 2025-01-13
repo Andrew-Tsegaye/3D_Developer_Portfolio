3ff2972383fc85bd6e8c7b4e04abbc2da1a9e510
Andrew-Tsegaye <andrewtsegaye7@gmail.com>
2025-01-13 15:19:17 +0300

    first commit

---
+++ src/constants/index.js
@@ -0,0 +1,325 @@
+export const navLinks = [
+  {
+    id: 1,
+    name: "Home",
+    href: "#home",
+  },
+  {
+    id: 2,
+    name: "About",
+    href: "#about",
+  },
+  {
+    id: 3,
+    name: "Work",
+    href: "#work",
+  },
+  {
+    id: 4,
+    name: "Contact",
+    href: "#contact",
+  },
+];
+
+export const clientReviews = [
+  {
+    id: 1,
+    name: "Ellie Fassman",
+    position: "PhD Researcher @ Cornell",
+    img: "assets/review1.png",
+    review:
+      "In addition to his intellectual capability, Andrew is a compassionate, driven, and collaborative. His determination to bring change to his community through technology and innovation makes him truly unique.",
+  },
+  {
+    id: 2,
+    name: "Mukerem Ali",
+    position: "Python Developer @ Turing",
+    img: "assets/review2.png",
+    review:
+      "Andrew Tsegaye, a skilled programmer with a strong grasp of Python and JavaScript. His ability to communicate effectively and collaborate with peers makes him a valuable asset in any technical environment.",
+  },
+  {
+    id: 3,
+    name: "Isaac Sahle",
+    position: "Software Engineer @ Google",
+    img: "assets/review4.png",
+    review:
+      "Andrew shows promising tenacity, dedication, and problem-solving skills. His eagerness to learn and take initiative positions him for success in software engineering. I anticipate his future achievements.",
+  },
+  {
+    id: 4,
+    name: "Georg Osang",
+    position: "Mathematical Scientist @ IDEMS",
+    img: "assets/review3.png",
+    review:
+      "Andrew’s passion for AI and web development shines through his diverse projects. He continuously strives to improve his skills, particularly in algorithmic problem-solving.",
+  },
+];
+
+export const myProjects = [
+  {
+    title: "Podcastr - AI Podcast Platform",
+    desc: "Podcastr is a revolutionary Software-as-a-Service platform that transforms the way podcasts are created. With advanced AI-powered features like text-to-multiple-voices functionality, it allows creators to generate diverse voiceovers from a single text input.",
+    subdesc:
+      "Built as a unique Software-as-a-Service app with Next.js 14, Tailwind CSS, TypeScript, Framer Motion and Convex, Podcastr is designed for optimal performance and scalability.",
+    href: "https://github.com/Andrew-Tsegaye/AI-Powered-Podcast-Platform",
+    texture: "/textures/project/project1.mp4",
+    logo: "/assets/project-logo1.png",
+    logoStyle: {
+      backgroundColor: "#2A1816",
+      border: "0.2px solid #36201D",
+      boxShadow: "0px 0px 60px 0px #AA3C304D",
+    },
+    spotlight: "/assets/spotlight1.png",
+    tags: [
+      {
+        id: 1,
+        name: "React.js",
+        path: "/assets/react.svg",
+      },
+      {
+        id: 2,
+        name: "TailwindCSS",
+        path: "assets/tailwindcss.png",
+      },
+      {
+        id: 3,
+        name: "TypeScript",
+        path: "/assets/typescript.png",
+      },
+      {
+        id: 4,
+        name: "Framer Motion",
+        path: "/assets/framer.png",
+      },
+    ],
+  },
+  {
+    title: "CarePulse - Health Management System",
+    desc: "An innovative healthcare platform designed to streamline essential medical processes. It simplifies patient registration, appointment scheduling, and medical record management, providing a seamless experience for both healthcare providers and patients.",
+    subdesc:
+      "With a focus on efficiency, CarePulse integrantes complex forms and SMS notifications, by using Next.js, Appwrite, Twillio and Sentry that enhance operational workflows.",
+    href: "https://github.com/Andrew-Tsegaye/Healthcare-Management-System",
+    texture: "/textures/project/project3.mp4",
+    logo: "/assets/project-logo3.png",
+    logoStyle: {
+      backgroundColor: "#60f5a1",
+      background:
+        "linear-gradient(0deg, #60F5A150, #60F5A150), linear-gradient(180deg, rgba(255, 255, 255, 0.9) 0%, rgba(208, 213, 221, 0.8) 100%)",
+      border: "0.2px solid rgba(208, 213, 221, 1)",
+      boxShadow: "0px 0px 60px 0px rgba(35, 131, 96, 0.3)",
+    },
+    spotlight: "/assets/spotlight3.png",
+    tags: [
+      {
+        id: 1,
+        name: "React.js",
+        path: "/assets/react.svg",
+      },
+      {
+        id: 2,
+        name: "TailwindCSS",
+        path: "assets/tailwindcss.png",
+      },
+      {
+        id: 3,
+        name: "TypeScript",
+        path: "/assets/typescript.png",
+      },
+      {
+        id: 4,
+        name: "Framer Motion",
+        path: "/assets/framer.png",
+      },
+    ],
+  },
+  {
+    title: "Horizon - Online Banking Platform",
+    desc: "Horizon is a comprehensive online banking platform that offers users a centralized finance management dashboard. It allows users to connect multiple bank accounts, monitor real-time transactions, and seamlessly transfer money to other users.",
+    subdesc:
+      "Built with Next.js 14 Appwrite, Dwolla and Plaid, Horizon ensures a smooth and secure banking experience, tailored to meet the needs of modern consumers.",
+    href: "https://github.com/Andrew-Tsegaye/Banking-App-with-Finance-Management-Dashboard",
+    texture: "/textures/project/project4.mp4",
+    logo: "/assets/project-logo4.png",
+    logoStyle: {
+      backgroundColor: "#0E1F38",
+      border: "0.2px solid #0E2D58",
+      boxShadow: "0px 0px 60px 0px #2F67B64D",
+    },
+    spotlight: "/assets/spotlight4.png",
+    tags: [
+      {
+        id: 1,
+        name: "React.js",
+        path: "/assets/react.svg",
+      },
+      {
+        id: 2,
+        name: "TailwindCSS",
+        path: "assets/tailwindcss.png",
+      },
+      {
+        id: 3,
+        name: "TypeScript",
+        path: "/assets/typescript.png",
+      },
+      {
+        id: 4,
+        name: "Framer Motion",
+        path: "/assets/framer.png",
+      },
+    ],
+  },
+  {
+    title: "LiveDoc - Real-Time Google Docs Clone",
+    desc: "LiveDoc is a powerful collaborative app that elevates the capabilities of real-time document editing. As an enhanced version of Google Docs, It supports millions of collaborators simultaneously, ensuring that every change is captured instantly and accurately.",
+    subdesc:
+      "With LiveDoc, users can experience the future of collaboration, where multiple contributors work together in real time without any lag, by using Next.js and Liveblocks newest features.",
+    href: "https://github.com/Andrew-Tsegaye/Collaborative-Editor-App",
+    texture: "/textures/project/project2.mp4",
+    logo: "/assets/project-logo2.png",
+    logoStyle: {
+      backgroundColor: "#13202F",
+      border: "0.2px solid #17293E",
+      boxShadow: "0px 0px 60px 0px #2F6DB54D",
+    },
+    spotlight: "/assets/spotlight2.png",
+    tags: [
+      {
+        id: 1,
+        name: "React.js",
+        path: "/assets/react.svg",
+      },
+      {
+        id: 2,
+        name: "TailwindCSS",
+        path: "assets/tailwindcss.png",
+      },
+      {
+        id: 3,
+        name: "TypeScript",
+        path: "/assets/typescript.png",
+      },
+      {
+        id: 4,
+        name: "Framer Motion",
+        path: "/assets/framer.png",
+      },
+    ],
+  },
+  {
+    title: "Imaginify - AI Photo Manipulation App",
+    desc: "Imaginify is a groundbreaking Software-as-a-Service application that empowers users to create stunning photo manipulations using AI technology. With features like AI-driven image editing, a payments system, and a credits-based model.",
+    subdesc:
+      "Built with Next.js 14, Cloudinary AI, Clerk, and Stripe, Imaginify combines cutting-edge technology with a user-centric approach. It can be turned into a side income or even a full-fledged business.",
+    href: "https://github.com/Andrew-Tsegaye/An-AI-SaaS-Platform",
+    texture: "/textures/project/project5.mp4",
+    logo: "/assets/project-logo5.png",
+    logoStyle: {
+      backgroundColor: "#1C1A43",
+      border: "0.2px solid #252262",
+      boxShadow: "0px 0px 60px 0px #635BFF4D",
+    },
+    spotlight: "/assets/spotlight5.png",
+    tags: [
+      {
+        id: 1,
+        name: "React.js",
+        path: "/assets/react.svg",
+      },
+      {
+        id: 2,
+        name: "TailwindCSS",
+        path: "assets/tailwindcss.png",
+      },
+      {
+        id: 3,
+        name: "TypeScript",
+        path: "/assets/typescript.png",
+      },
+      {
+        id: 4,
+        name: "Framer Motion",
+        path: "/assets/framer.png",
+      },
+    ],
+  },
+];
+
+export const calculateSizes = (isSmall, isMobile, isTablet) => {
+  return {
+    deskScale: isSmall ? 0.05 : isMobile ? 0.06 : 0.065,
+    deskPosition: isMobile ? [0.5, -4.5, 0] : [0.25, -5.5, 0],
+    cubePosition: isSmall
+      ? [4, -5, 0]
+      : isMobile
+      ? [5, -5, 0]
+      : isTablet
+      ? [5, -5, 0]
+      : [9, -5.5, 0],
+    reactLogoPosition: isSmall
+      ? [3, 4, 0]
+      : isMobile
+      ? [5, 4, 0]
+      : isTablet
+      ? [5, 4, 0]
+      : [12, 3, 0],
+    ringPosition: isSmall
+      ? [-5, 7, 0]
+      : isMobile
+      ? [-10, 10, 0]
+      : isTablet
+      ? [-12, 10, 0]
+      : [-24, 10, 0],
+    targetPosition: isSmall
+      ? [-5, -10, -10]
+      : isMobile
+      ? [-9, -10, -10]
+      : isTablet
+      ? [-11, -7, -10]
+      : [-13, -13, -10],
+  };
+};
+
+export const workExperiences = [
+  {
+    id: 1,
+    name: "Freelancer",
+    pos: "Junior Full Stack Developer",
+    duration: "September 2023 - Present",
+    title:
+      "Designing and developing scalable, responsive websites and web applications using Next.js, TypeScript, Node.js, MongoDB, Appwrite, Supabase, Tailwind CSS, and NextAuth. Delivered high-quality, user-centric solutions while managing end-to-end development as a freelancer.",
+    icon: "/assets/freelancer.svg",
+    animation: "salute",
+  },
+  {
+    id: 2,
+    name: "Lumiere Research Scholar",
+    pos: "Deep Learning Researcher",
+    duration: "June 2024 - August 2024",
+    title:
+      "Researched real-time automatic license plate recognition using deep learning, developing and optimizing models for precise and efficient detection. Integrated advanced computer vision techniques and deep learning concepts, including object tracking, to enhance overall recognition performance.",
+    icon: "/assets/lumiere.png",
+    animation: "clapping",
+  },
+  {
+    id: 3,
+    name: "SmartStickies",
+    pos: "Artificial Intelligence Engineer Intern",
+    duration: "March 2024 - May 2024",
+    title:
+      "Artificial Intelligence Engineer intern at SmartStickies, where I built an AI-powered Screen Workflow Generator from scratch, developed advanced AI models, and worked on GitHub AI Assistant & Advanced AI Code Generation Agent—demonstrated skills in Python programming, AI, LLMs, and NLP algorithms.",
+    icon: "/assets/smartstickies.png",
+    animation: "victory",
+  },
+  {
+    id: 4,
+    name: "Robox Robotics and Artificial Intelligence Center",
+    pos: "Mentor & Student",
+    duration: "June 2022 - August 2022",
+    title:
+      "Developed an autonomous cleaning robot, learned AI and Arduino programming, and assisted younger students with Python by explaining and debugging codes. This experience honed my problem-solving skills, deepened my tech awareness, and involved building and assembling robots for hands-on projects.",
+    icon: "/assets/robox.png",
+    animation: "salute",
+  },
+];
